7c609a8253538ae157360f5b21002aa87e8044d2
Adeline Gr <adeline.granet@gmail.com>
2014-02-11 00:16:37 +0100

    correction

--- classifieur/src/tweetParser/Tweet.java
+++ classifieur/src/tweetParser/Tweet.java
@@ -1,5 +1,9 @@
 ﻿package tweetParser;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
